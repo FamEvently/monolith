7962e7fdb9224b1801221c31392c840017496dd9
Yavor Ganev <yavor.ganev@trading212.com>
2025-10-28 09:52:25 +0200

    initial commit

---
+++ src/test/java/com/famevently/monolith/MonolithApplicationTests.java
@@ -0,0 +1,15 @@
+package com.famevently.monolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class MonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
