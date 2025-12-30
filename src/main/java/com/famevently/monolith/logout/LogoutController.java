6b5535d4f80a75f8fbc71e146efc503829167a6d
y.ganev <yavorgnv@gmail.com>
2025-12-30 15:58:55 +0200

    refactor

--- src/main/java/com/famevently/monolith/logout/LogoutController.java
+++ src/main/java/com/famevently/monolith/logout/LogoutController.java
@@ -2,7 +2,6 @@ package com.famevently.monolith.logout;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
