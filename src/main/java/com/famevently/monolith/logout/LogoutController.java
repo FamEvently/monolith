fed384d6f5a5ba9a771262316f6611ffc7a56dc6
y.ganev <yavorgnv@gmail.com>
2025-12-30 15:58:36 +0200

    logout logic

---
+++ src/main/java/com/famevently/monolith/logout/LogoutController.java
@@ -0,0 +1,23 @@
+package com.famevently.monolith.logout;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogoutController
+{
+    private final LogoutService logoutService;
+
+    public LogoutController(LogoutService logoutService) {
+        this.logoutService = logoutService;
+    }
+
+    @DeleteMapping("/v1/users/{userId}/devices/{deviceUuid}/logout")
+    public void logout(@PathVariable final Long userId,
+                       @PathVariable final String deviceUuid)
+    {
+        logoutService.logout(userId, deviceUuid);
+    }
+}
