4bde6adecfaf245eb9f16882bbf587632b5fdc13
Snowflakyy <148199406+Snowflakyy@users.noreply.github.com>
2026-01-19 09:33:23 +0200

    Revert "Add LoginToken and handle exceptions (#31)"
    This reverts commit 8ffc55a54ee965b2f736f979956d357ff6af25fd.

--- src/main/java/com/famevently/monolith/logout/LogoutService.java
+++ src/main/java/com/famevently/monolith/logout/LogoutService.java
@@ -1,22 +1,19 @@
 package com.famevently.monolith.logout;
 
-import com.famevently.monolith.logintoken.LoginTokenService;
 import com.famevently.monolith.usersession.UserSessionRestClient;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LogoutService {
     private final UserSessionRestClient userSessionRestClient;
-    private final LoginTokenService loginTokenService;
 
-    public LogoutService(final UserSessionRestClient userSessionRestClient, final LoginTokenService loginTokenService) {
+    public LogoutService(final UserSessionRestClient userSessionRestClient) {
         this.userSessionRestClient = userSessionRestClient;
-        this.loginTokenService = loginTokenService;
     }
 
     public void logout(final Long userId, final String deviceUuid)
     {
         userSessionRestClient.deleteSession(deviceUuid);
-        loginTokenService.deleteTokenByUserId(userId);
+        //delete login token
     }
 }
